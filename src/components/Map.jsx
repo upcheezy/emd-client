473bf37706111d29fed3ad99d6dd416c22e1f3e0
Matthew Upchurch <upcheezy@gmail.com>
2020-05-28 14:38:55 -0400

    address search and drawing are working

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
-import './Map.css';
-import mapboxgl from 'mapbox-gl';
-import ReactMapboxGl from 'react-mapbox-gl';
-import DrawControl from 'react-mapbox-gl-draw';
-import config from '../config';
-// var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
-import * from '@mapbox/mapbox-gl-geocoder';
+import React, { Component } from "react";
+import "./Map.css";
+import mapboxgl from "mapbox-gl";
+import ReactMapboxGl from "react-mapbox-gl";
+// import * as MapboxDraw from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw';
+import config from "../config";
+const MapboxDraw = require("@mapbox/mapbox-gl-draw");
+const MapboxGeocoder = require("@mapbox/mapbox-gl-geocoder");
 
 export default class Map extends Component {
-    
-    componentDidMount() {
-        mapboxgl.accessToken = config.MAPBOX_API_TOKEN
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-79.4512, 43.6568],
-            zoom: 13
-            });
-            map.addControl(
-                    new MapboxGeocoder({
-                    accessToken: mapboxgl.accessToken,
-                    mapboxgl: mapboxgl
-                })
-            );
-    }
+  componentDidMount() {
+    mapboxgl.accessToken = config.MAPBOX_API_TOKEN;
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-79.4512, 43.6568],
+      zoom: 13,
+    });
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      })
+    );
+    const draw = new MapboxDraw({
+      displayControlsDefault: false,
+      controls: {
+        polygon: true,
+        trash: true,
+      },
+    });
+    map.addControl(draw);
+  }
 
-    render() {
-        return (
-            <div id='map'>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="map">
+        <div className="calculation-box">
+          <p>Draw a polygon using the draw tools.</p>
+          <div id="calculated-area"></div>
+        </div>
+      </div>
+    );
+  }
 }
