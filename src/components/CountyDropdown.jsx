dd9e3db8e2bdd698d6320188ee8391c4b52e7b18
Matthew Upchurch <upcheezy@gmail.com>
2020-06-05 12:10:59 -0400

    trying to access event inside cdm

--- src/components/CountyDropdown.jsx
+++ src/components/CountyDropdown.jsx
@@ -1,62 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class CountyDropdown extends Component {
-    state = {
-        counties: [],
-        value: '',
-        error: null
-    }
+  state = {
+    counties: [],
+    value: "",
+    error: null,
+  };
 
-    setCounties = counties => {
-        this.setState({
-            counties,
-            error: null
-        })
-    }
+  setCounties = (counties) => {
+    this.setState({
+      counties,
+      error: null,
+    });
+  };
 
-    componentDidMount() {
-        fetch('http://localhost:8000/counties', {
-            method: "GET",
-            headers: {
-                "content-type": "application/json"
-            }
-        })
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(res.status);
-                }
-                return res.json();
-            })
-            .then(this.setCounties)
-            .catch(error => this.setState({error}));
-    }
+  componentDidMount() {
+    fetch("http://localhost:8000/counties", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then(this.setCounties)
+      .catch((error) => this.setState({ error }));
+  }
 
-    handleDropdownChange = (e) => {
-        this.setState({value: e.target.value})
-        console.log(this.state.value)
-    }
+  handleDropdownChange = (e) => {
+    // console.log(e.target.value)
+    this.setState(
+      {
+        value: e.target.value,
+      },
+      () => {
+        this.props.onAction(this.state.value);
+      }
+    );
+    // console.log(this.state.value)
+  };
 
-    render() {
-        // console.log(this.props)
-        return (
-            <form className='county-dropdown' >
-                <label htmlFor="County Search"></label>
-                <select 
-                    name="county search" 
-                    id="county search"
-                    onChange={this.handleDropdownChange}
-                >
-                    <option value="County Select" selected disabled hidden>County Select</option>
-                    {this.state.counties.map(county => {
-                        return (
-                            <>
-                                <option value="{county.countyname}">{county.countyname}</option>
-                            </>
-                        )
-                    })}
-                </select>
-            </form>
-        )
-    }
+  render() {
+    // console.log(this.props)
+    return (
+      <form className="county-dropdown">
+        <label htmlFor="County Search"></label>
+        <select
+          name="county search"
+          id="county search"
+          onChange={this.handleDropdownChange}
+        >
+          <option value="County Select" selected disabled hidden>County Select</option>
+          {this.state.counties.map((county) => {
+            return (
+              <>
+                <option value={county.countyname}>{county.countyname}</option>
+              </>
+            );
+          })}
+        </select>
+      </form>
+    );
+  }
 }
-
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -20,28 +20,6 @@ export default class Map extends Component {
       zoom: 7,
     });
 
-    const fetchCountyname = (name) => {
-      fetch("http://localhost:8000/countyselect", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          countyname: name,
-        }),
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(res.status);
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => this.setState({ error }));
-    };
-
     const fetchIntersect = (datapoints, type) => {
       fetch("http://localhost:8000/draw", {
         method: "POST",
@@ -89,83 +67,62 @@ export default class Map extends Component {
       fetchIntersect(data.features[0].geometry.coordinates[0], "draw");
     };
 
+    const layerSwitcher = (e) => {
+      let layerId = e.target.id;
+      map.setStyle('mapbox://styles/mapbox/' + layerId)
+      console.log(e.target.id)
+    }
+
     map.on("draw.create", updateArea);
     map.on("draw.delete", updateArea);
-
-    map.on("load", function () {
-      map.addSource("maine", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          geometry: {
-            type: "Polygon",
-            coordinates: [
-              [
-                [-67.13734351262877, 45.137451890638886],
-                [-66.96466, 44.8097],
-                [-68.03252, 44.3252],
-                [-69.06, 43.98],
-                [-70.11617, 43.68405],
-                [-70.64573401557249, 43.090083319667144],
-                [-70.75102474636725, 43.08003225358635],
-                [-70.79761105007827, 43.21973948828747],
-                [-70.98176001655037, 43.36789581966826],
-                [-70.94416541205806, 43.46633942318431],
-                [-71.08482, 45.3052400000002],
-                [-70.6600225491012, 45.46022288673396],
-                [-70.30495378282376, 45.914794623389355],
-                [-70.00014034695016, 46.69317088478567],
-                [-69.23708614772835, 47.44777598732787],
-                [-68.90478084987546, 47.184794623394396],
-                [-68.23430497910454, 47.35462921812177],
-                [-67.79035274928509, 47.066248887716995],
-                [-67.79141211614706, 45.702585354182816],
-                [-67.13734351262877, 45.137451890638886],
-              ],
-            ],
-          },
-        },
-      });
-      map.addLayer({
-        id: "maine",
-        type: "fill",
-        source: "maine",
-        layout: {},
-        paint: {
-          "fill-color": "#088",
-          "fill-opacity": 0.8,
-        },
-      });
-    });
   }
 
   funcCheker(name) {
     console.log(name)
-    // fetch("http://localhost:8000/countyselect", {
-    //     method: "POST",
-    //     headers: {
-    //       "content-type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       countyname: name,
-    //     }),
-    //   })
-    //     .then((res) => {
-    //       if (!res.ok) {
-    //         throw new Error(res.status);
-    //       }
-    //       return res.json();
-    //     })
-    //     .then((data) => {
-    //       console.log(data);
-    //     })
-    //     .catch((error) => this.setState({ error }));
+    fetch("http://localhost:8000/countyselect", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          countyname: name,
+        }),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(res.status);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => this.setState({ error }));
   }
 
   render() {
     return (
       <div className="container">
         <div id="map"></div>
+        <div className='layerMenu'>
+          <input 
+            type="radio"
+            id="streets-v11"
+            value="streets"
+            checked="checked"
+            name="rtoggle"
+            onClick={(e) => this.layerSwitcher(e)}
+          />
+          <label htmlFor="streets-v11">Streets</label>
+          <input
+            type="radio"
+            id="satellite-v9"
+            name="rtoggle"
+            value="satellite"
+            onClick={(e) => this.layerSwitcher(e)}
+          />
+          <label htmlFor="satellite-v9">Satellite</label>
+        </div>
         <CountyDropdown onAction={this.funcCheker} />
       </div>
     );
