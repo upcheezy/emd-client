85cf84af2b7ec41ec4051ff66d95432cccfd7f53
Matthew Upchurch <upcheezy@gmail.com>
2020-06-13 22:01:32 -0400

    rebuit geojson feature layer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,7 +12,82 @@ export default class Map extends Component {
     error: null,
     members: [],
     grid: [],
+    drawCoords: "",
   };
+
+  fetchGrid() {
+    fetch("http://gis17-01:8000/grid", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        let gridRowId = window.localStorage.getItem("id").split(",");
+        // console.log(data)
+        // console.log(gridRowId)
+        let matching = [];
+        gridRowId.map((element) => {
+          const idContains = data.rows.find(
+            (id_element) => id_element.id === parseInt(element)
+          );
+          if (typeof idContains !== "undefined") matching.push(idContains);
+        });
+        console.log(matching)
+        const gridGeom = matching.map((g) => Parse(g.geom));
+        console.log(gridGeom)
+        // let gridParse = Parse(matching[0].geom);
+        const gridCoords = gridGeom.map((gg) => gg.coordinates[0][0]);
+        console.log(gridCoords);
+        let gj = {
+          "type": "FeatureCollection",
+          "features": []
+        }
+        matching.forEach((element) => {
+          gj.features.push(
+            {
+              "type": "Feature",
+              "properties": {
+                "id": element.id
+              },
+              "geometry": {
+                "type": "Polygon",
+                "coordinates":[Parse(element.geom).coordinates[0][0]]
+              }
+            }
+          )
+        })
+        // if (window.map.getLayer('maine')) window.map.removeLayer('maine');
+        window.map.addSource("maine", {
+          type: "geojson",
+          data: gj,
+        });
+        window.map.addLayer({
+          id: "maine",
+          type: "fill",
+          source: "maine",
+          layout: {
+            // "text-field": ['get', 'id'],
+            // "text_font": [
+            //   "DIN Offc Pro Medium",
+            //   "Arial Unicode MS Bold"
+            // ],
+            // "text-size": 12
+          },
+          paint: {
+            "fill-color": "#088",
+            "fill-opacity": 0.8,
+          },
+        });
+      })
+      .catch((error) => this.setState({ error }));
+  }
 
   componentDidMount() {
     mapboxgl.accessToken = config.MAPBOX_API_TOKEN;
@@ -25,61 +100,7 @@ export default class Map extends Component {
     // call fetchgrid and get the grid id and the geom
     // then convert the geom into geojson and save it as a variable to pass
     // to map.fitbounds
-    const fetchGrid = () => {
-      fetch("http://gis17-01:8000/grid", {
-        method: "GET",
-        headers: {
-          "content-type": "application/json",
-        },
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(res.status);
-          }
-          return res.json();
-        })
-        .then((data) => {
-          let gridRowId = window.localStorage.getItem('id').split(',')
-          // console.log(data.rows)
-          // console.log(gridRowId)
-          let matching = [];
-          gridRowId.map((element) => {
-            const idContains = data.rows.find(id_element => id_element.id === parseInt(element))
-            if (typeof idContains !== 'undefined') matching.push(idContains)
-          })
-          // console.log(matching)
-          const gridGeom = matching.map(g => Parse(g.geom))
-          // console.log(gridGeom)
-          // let gridParse = Parse(matching[0].geom);
-          const gridCoords = gridGeom.map(gg => gg.coordinates[0][0])
-          // console.log(gridCoords);
-          // if (window.map.getLayer('maine')) window.map.removeLayer('maine');
-          window.map.addSource("maine", {
-            type: "geojson",
-            data: {
-              type: "Feature",
-              geometry: {
-                type: "MultiPolygon",
-                coordinates: [gridCoords],
-              },
-            },
-          });
-          window.map.addLayer({
-            id: "maine",
-            type: "fill",
-            source: "maine",
-            layout: {},
-            paint: {
-              "fill-color": "#088",
-              "fill-opacity": 0.8,
-            },
-          });
-          
-        })
-        .catch((error) => this.setState({ error }));
-    };
 
-    
     const fetchIntersect = (datapoints, type) => {
       fetch("http://gis17-01:8000/draw", {
         method: "POST",
@@ -104,37 +125,37 @@ export default class Map extends Component {
             // console.log(row.id_array)
             row.id_array.forEach((gid) => {
               // console.log(gid)
-              if(!gridIdz[gid]){
-                console.log(gridIdz[gid])
-                gridIdz[gid] = 1
+              if (!gridIdz[gid]) {
+                console.log(gridIdz[gid]);
+                gridIdz[gid] = 1;
               }
-            })
-          })
-          console.log(Object.keys(gridIdz))
+            });
+          });
+          console.log(Object.keys(gridIdz));
           // console.log(data);
           this.setState({
             members: Object.values(data.rows),
           });
-          window.localStorage.setItem('id', null)
-          if(window.localStorage.getItem('id')){
-            console.log('inside if')
-            window.localStorage.removeItem('id')
+          window.localStorage.setItem("id", null);
+          if (window.localStorage.getItem("id")) {
+            console.log("inside if");
+            window.localStorage.removeItem("id");
           }
-          window.localStorage.setItem('id',Object.keys(gridIdz))
-          fetchGrid();
+          window.localStorage.setItem("id", Object.keys(gridIdz));
+          this.fetchGrid();
         })
         .catch((error) => this.setState({ error }));
     };
-    
+
     const address = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl,
-      countries: 'us',
-      bbox: [-83.726807,31.784217,-78.013916,35.415915]
+      countries: "us",
+      bbox: [-83.726807, 31.784217, -78.013916, 35.415915],
     }).on("result", function ({ result }) {
       // console.log(result.geometry.coordinates);
       fetchIntersect(result.geometry.coordinates, "point");
-      fetchGrid();
+      // this.fetchGrid();
     });
     window.map.addControl(address);
 
@@ -148,24 +169,30 @@ export default class Map extends Component {
     window.map.addControl(draw);
 
     const updateArea = (e) => {
-      // let dataId
-      // if (draw.getAll().features.length > 1) draw.deleteAll()
+      console.log(e.features);
+      // if (draw.getAll().features.length > 1) draw.delete()
       const data = draw.getAll();
       // dataId = draw.getAll().features[0].id
-      // console.log(dataId)
-
+      console.log(data.features.length);
       console.log(data, data.features);
       if (data.features.length > 0) {
         fetchIntersect(data.features[0].geometry.coordinates[0], "draw");
       }
-      
-      
     };
 
     // let gCoords = this.state;
-    window.map.on("draw.create", updateArea);
-    window.map.on("draw.delete", updateArea);
-    window.map.on('draw.update', updateArea);
+    window.map.on("draw.create", (ev) => {
+      console.log(ev.features[0].id);
+      draw.delete(this.state.drawCoords);
+      this.setState({ drawCoords: ev.features[0].id });
+      let coordinates = ev.features[0].geometry.coordinates;
+      console.log([coordinates[0][0], coordinates[0][2]])
+      fetchIntersect(ev.features[0].geometry.coordinates[0], "draw");
+    });
+    window.map.on("draw.delete", () => {
+      console.log("deleted");
+    });
+    window.map.on("draw.update", updateArea);
   }
 
   countyChecker(name) {
@@ -189,25 +216,25 @@ export default class Map extends Component {
         console.log(Object.values(data.rows));
         // console.log(typeof data.values)
         const gridIdz = {};
-          data.rows.forEach((row) => {
-            // console.log(row.id_array)
-            row.id_array.forEach((gid) => {
-              // console.log(gid)
-              if(!gridIdz[gid]){
-                console.log(gridIdz[gid])
-                gridIdz[gid] = 1
-              }
-            })
-          })
-          console.log(Object.keys(gridIdz))
+        data.rows.forEach((row) => {
+          // console.log(row.id_array)
+          row.id_array.forEach((gid) => {
+            // console.log(gid)
+            if (!gridIdz[gid]) {
+              console.log(gridIdz[gid]);
+              gridIdz[gid] = 1;
+            }
+          });
+        });
+        console.log(Object.keys(gridIdz));
         this.setState({ members: Object.values(data.rows) });
-        window.localStorage.setItem('id', null)
-        if(window.localStorage.getItem('id')){
-          console.log('inside if')
-          window.localStorage.removeItem('id')
+        window.localStorage.setItem("id", null);
+        if (window.localStorage.getItem("id")) {
+          console.log("inside if");
+          window.localStorage.removeItem("id");
         }
-        window.localStorage.setItem('id',Object.keys(gridIdz))
-        // fetchGrid();
+        window.localStorage.setItem("id", Object.keys(gridIdz));
+        this.fetchGrid();
       })
       .catch((error) => this.setState({ error }));
   }
@@ -248,6 +275,7 @@ export default class Map extends Component {
           <h1>Affected Members</h1>
           <section className="member_list">
             <ul>
+              {console.log(this.state.members)}
               {this.state.members.map((x) => (
                 // give li class to style
                 <li className="member_li">
